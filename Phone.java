5178d0d22f0efdc816add7b63a64c84dbf4393f9
Ho Wee Sern <72723621+hws0406@users.noreply.github.com>
2021-03-31 16:22:32 +0800

    Create Phone.java

---
+++ Phone.java
@@ -0,0 +1,13 @@
+
+public class Phone {
+
+		String brand;
+		String operatingSystem;
+		String processorType;
+		String memoryGB;
+		
+		void Function() {
+			String Function = ("GPS \n" + "Internet \n" + "changeVolume \n" + "powerOff \n" + "powerOnn");
+			System.out.println("Function : \n" + Function);
+		}
+	}
